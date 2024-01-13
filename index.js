c9ce48bf3ba2dbe725d0e5955e4dd1cf9c4bb188
devlrakib <devrakib5@gmail.com>
2024-01-14 01:36:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// For Loop
+for(var i=1; i<=10; i++){
+    document.write(" "+i+"</br>");
+}
+
+// While Loop
+var i=11;
+while(i<=30){
+    document.write(" "+i);
+    i++;
+}
+
+//Do While
+var i=31;
+do{
+  document.write(" "+i);
+  i++;
+}while(i<=50);
